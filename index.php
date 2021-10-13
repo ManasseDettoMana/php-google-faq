21b9f4ff5bc44c8edf5b184e99a3edf7ace36789
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-13 21:54:07 +0200

    reset tolto

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             "risposta" =>'
                 <p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
                 <ol>
-                    <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i  propri servizi per i consumatori tramite una delle due società seguenti:
+                    <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                         <ul>
                             <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                             <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
@@ -70,6 +70,9 @@
     <title>Document</title>
 </head>
 <body>
+    <header>
+
+    </header>
     <div class="container">
         <?php foreach($faq as $domande){ ?>
         <h2><?php echo $domande['domanda']; ?></h2>
